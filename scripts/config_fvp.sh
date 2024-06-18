738f68da6397132e356046b01bbc670a77849860
Zheng-Bicheng <zheng_bicheng@outlook.com>
2024-06-18 14:17:18 +0800

    update

--- scripts/config_fvp.sh
+++ scripts/config_fvp.sh
@@ -6,4 +6,4 @@ rm -rf avh-linux-aarch64_11.26_11_Linux64_armv8l.tgz
 cp -r avh-linux-aarch64/bin/* /opt/VHT/bin/
 rm -rf avh-linux-aarch64
 
-FVP_Corstone_SSE-300 --version
+/opt/VHT/bin/FVP_Corstone_SSE-300 --version
